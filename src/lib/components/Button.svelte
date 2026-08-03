4eeed983e250b8fb5c0290d6bdaa21b6e8f6a26b
Engin Ünel <engin@macbook.local>
2026-08-03 14:07:28 +0200

    [+] Button, Card components and class utils

---
+++ src/lib/components/Button.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import { cn } from '$lib/utils';
+	import type { Snippet } from 'svelte';
+
+	type Props = {
+		children: Snippet<[]>;
+		class?: string;
+	};
+
+	let { children, class: className = '' }: Props = $props();
+</script>
+
+<button
+	class={cn(
+		`cursor-pointer border border-gray-300 bg-white px-5 py-3 font-mono uppercase`,
+		className
+	)}
+>
+	{@render children()}
+</button>
---
+++ src/lib/components/Card.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+	import type { Snippet } from 'svelte';
+
+	type Props = {
+		header?: Snippet;
+		children?: Snippet;
+		footer?: Snippet;
+	};
+
+	let { header, children, footer }: Props = $props();
+</script>
+
+<article class="border border-gray-300 bg-white *:p-4">
+	{#if header}
+		<header class="flex justify-between">
+			{@render header()}
+		</header>
+	{/if}
+	{#if children}
+		<div class="border-t border-gray-300">
+			{@render children()}
+		</div>
+	{/if}
+	{#if footer}
+		<footer class="flex gap-4 border-t border-gray-300">
+			{@render footer()}
+		</footer>
+	{/if}
+</article>
