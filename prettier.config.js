a61d9f79f0d568e467ac486a495bdc10137dfbfc
Engin Ünel <engin@macbook.local>
2026-08-03 14:06:06 +0200

    Init

---
+++ prettier.config.js
@@ -0,0 +1,13 @@
+/** @type {import("prettier").Config} */
+const config = {
+	useTabs: true,
+	// tabWidth: 4,
+	singleQuote: false,
+	trailingComma: "none",
+	printWidth: 100,
+	plugins: ["prettier-plugin-svelte", "prettier-plugin-tailwindcss"],
+	overrides: [{ files: "*.svelte", options: { parser: "svelte" } }],
+	tailwindStylesheet: "./src/routes/layout.css"
+};
+
+export default config;
